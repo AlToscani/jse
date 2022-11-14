e924590a0a98d212e49ab5f9d3e6caadd8c13c3c
AlToscani <93136804+AlToscani@users.noreply.github.com>
2022-11-14 16:01:58 +0100

    es

--- src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
+++ src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
@@ -1,13 +1,27 @@
 package com.example.jse.m02.s12.ex;
 
 public class Exercise4 {
-    public static void main(String[] args) {
-        /*
-         * TODO: print up to 3 args
-         * 
-         * ["ciao", "hello"] -> "ciao" "hello"
-         * 
-         * ["ciao", "hello", "ciao", "hello"] -> "ciao" "hello", "ciao"
-         */
-    }
+	public static void main(String[] args) {
+		/*
+		 * TODO: print up to 3 args
+		 * 
+		 * ["ciao", "hello"] -> "ciao" "hello"
+		 * 
+		 * ["ciao", "hello", "ciao", "hello"] -> "ciao" "hello", "ciao"
+		 */
+
+//		
+//		for (int i = 0; i < args.length; i++) {
+//			System.out.println(args[i]);
+//			if (i == 2) {
+//				System.out.println("no more print for u, bye");
+//				break;
+//			}
+//		}
+//		
+		int len = args.length < 3 ? args.length : 3;
+		for (int i = 0; i < len; i++) {
+			System.out.println(args[i]);
+		}
+	}
 }
