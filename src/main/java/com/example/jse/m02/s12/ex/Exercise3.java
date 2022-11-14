53eda381c4fbd9df6b177577e4c45078fdcafb1b
AlToscani <93136804+AlToscani@users.noreply.github.com>
2022-11-14 15:48:15 +0100

    es

--- src/main/java/com/example/jse/m02/s12/ex/Exercise3.java
+++ src/main/java/com/example/jse/m02/s12/ex/Exercise3.java
@@ -1,11 +1,16 @@
 package com.example.jse.m02.s12.ex;
 
 public class Exercise3 {
-    public static void main(String[] args) {
-        /*
-         * TODO: Print 
-         * (1) the number of arguments passed to this main
-         * (2) each argument
-         */
-    }
+	public static void main(String[] args) {
+		/*
+		 * TODO: Print (1) the number of arguments passed to this main (2) each argument
+		 */
+		System.out.println("the length of args is: " + args.length);
+		System.out.println("here's the content:");
+		if (args.length != 0) {
+			for (String arg : args) {
+				System.out.print(arg + ", ");
+			}
+		}
+	}
 }
