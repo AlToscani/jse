92c3cc1bfd679f2f70c25439174b906e7aa0b85d
AlToscani <93136804+AlToscani@users.noreply.github.com>
2022-11-15 13:20:05 +0100

    es

--- src/main/java/com/example/jse/m03/s05/ex/Exercise.java
+++ src/main/java/com/example/jse/m03/s05/ex/Exercise.java
@@ -1,33 +1,70 @@
 package com.example.jse.m03.s05.ex;
 
 public class Exercise {
-//    private static final int GAP = 'a' - 'A';     // charset independent 
-//    private static final int ALPHABET_SIZE = 26;  // standard alphabet
+	private static final int GAP = 'a' - 'A'; // charset independent
+	private static final int ALPHABET_SIZE = 26; // standard alphabet
 
-    /**
-     * A simpler version of String.toUpperCase()
-     * 
-     * Build the result in a StringBuilder, then convert it to String and return it
-     * 
-     * Do not use any method in String!
-     * 
-     * @param s
-     * @return uppercase version of input
-     */
-    public String toUpper(String s) {
-        // TODO: create an uppercase version of the input and return it
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+	/**
+	 * A simpler version of String.toUpperCase()
+	 * 
+	 * Build the result in a StringBuilder, then convert it to String and return it
+	 * 
+	 * Do not use any method in String!
+	 * 
+	 * @param s
+	 * @return uppercase version of input
+	 */
+	public String toUpper(String s) {
 
-    /**
-     * Encrypt an uppercase string using the Caesar's algorithm
-     * 
-     * @param s     a string (uppercase only)
-     * @param shift encrypting key
-     * @return the encrypted string
-     */
-    public String caesarEncrypt(String s, int shift) {
-        // TODO: create an encrypted version of the input and return it
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+		if (s == null || s.isEmpty()) {
+			return s;
+		}
+
+		StringBuilder result = new StringBuilder("");
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c >= 'a' && c <= 'z') {
+//				c = (char) (c - GAP); //altro modo
+				c -= GAP;
+			}
+			result.append(c);
+		}
+
+		return result.toString();
+	}
+
+	/**
+	 * Encrypt an uppercase string using the Caesar's algorithm
+	 * 
+	 * @param s     a string (uppercase only)
+	 * @param shift encrypting key
+	 * @return the encrypted string
+	 */
+	public String caesarEncrypt(String s, int shift) {
+
+		if (s == null || s.isEmpty()) {
+			return s;
+		}
+
+		StringBuilder result = new StringBuilder("");
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c >= 'A' && c <= 'Z') {
+				c += (shift % ALPHABET_SIZE);
+				if (c < 'A') {
+					int exceeding = c - 'A';
+					c = 'Z' + 1;
+					c += exceeding;
+				} else if (c > 'Z') {
+					int exceeding = c - 'Z';
+					c = 'A' - 1;
+					c += exceeding;
+				}
+			}
+			result.append(c);
+		}
+		return result.toString();
+	}
 }
